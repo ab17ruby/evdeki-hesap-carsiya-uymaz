ea378a3ae40e3f56f802e955cc0e5016178c80de
emredo <himmet172@gmail.com>
2017-02-07 16:18:06 +0200

    Add files via upload

--- calistirma.rb
+++ calistirma.rb
@@ -1,25 +1,79 @@
 require './program'
-while true
-    print "urun adi giriniz"
-    a=gets.chomp()
-    print "urun magazasi giriniz"
-    b=gets.chomp()
-    print "urun miktari girinz"
-    c=gets.chomp()
-    print "urun fiyati giri,niz "
-    d=gets.chomp()
-    print " "
-    e=gets.chomp()
-    print "1,2"
-    adli=gets.chomp()
-    if adli== "2"
+
+puts `clear`
+puts "Welcome to my alışveriş sepeti"
+loop do
+  puts "seçimler"
+  puts "1) Yeni ekle"
+  puts "2) Tahmini Fiyat Toplam ve Cikis"
+  puts "3) Gercek fiyati"
+  puts "4) Gercek Fiyat Toplam ve Cikis"
+  puts "5) Farki gorme"
+  puts "6) Evdeki Hesap"
+
+  case gets.chomp
+    when "1"
+      print "urun adi giriniz :\t"
+      urunadi = gets.chomp()
+      print "urun magazasi giriniz :\t"
+      urunmagazasi=gets.chomp()
+      print "urun miktari giriniz :\t"
+      urunmiktari=gets.chomp()
+      print "urun fiyati giriniz :\t"
+      urunfiyati=gets.chomp()
+      print "urun tarihi giriniz :\t"
+      uruntarihi=gets.chomp()
+      ali=Alisveris_listesi::Urun.new
+      ali.urun_adi = urunadi
+      ali.urun_magazasi=urunmagazasi
+      ali.urun_miktari=urunmiktari
+      ali.urun_fiyati=urunfiyati
+      ali.urun_tarihi=uruntarihi
+      ali.ekle()
+      puts "ürün eklendi"
+
+    when "2"
+      puts "#{Alisveris_listesi::Urun.fiyat_toplama} tutarinda tahmini fiyatiniz vardir"
       break
+    when "3"
+      print "urun adi giriniz :\t"
+      urunadi = gets.chomp()
+      print "urun magazasi giriniz :\t"
+      urunmagazasi=gets.chomp()
+      print "urun miktari giriniz :\t"
+      urunmiktari=gets.chomp()
+      print "urun fiyati giriniz :\t"
+      urunfiyati=gets.chomp()
+      print "urun tarihi giriniz :\t"
+      uruntarihi=gets.chomp()
+      print "gercek fiyati gir :\t"
+      gercekfiyat=gets.chomp()
+      kali=Alisveris_listesi::Urun.new
+      kali.urun_adi = urunadi
+      kali.urun_magazasi=urunmagazasi
+      kali.urun_miktari=urunmiktari
+      kali.urun_fiyati=urunfiyati
+      kali.urun_tarihi=uruntarihi
+      kali.gercek_fiyati=gercekfiyat
+      kali.gercek_fiyatlar
+      puts "urun gercek fiyatlari belirlendi"
+    when "4"
+      puts "#{Alisveris_listesi::Urun.gercek_fiyat_toplama} tutarinda gercek fiyatiniz vardir"
+      break
+    when "5"
+      puts "#{Alisveris_listesi::Urun.gercek_fiyat_toplama - Alisveris_listesi::Urun.fiyat_toplama} gerçek fiyatlar tahminden farkı var"
+      break
+    when "6"
+      case Alisveris_listesi::Urun.gercek_fiyat_toplama <=> Alisveris_listesi::Urun.fiyat_toplama
+      when 0
+        puts "Evdeki hesap çarşıya uydu"
+        break
+      when 1
+        puts "Evdeki hesap çarşıya uymadı"
+        break
+      when -1
+        puts "Evdeki hesap iyiki çarşıya uymadı"
+        break
+      end
     end
-    ali=Alisveris_listesi::Urun.new
-    ali.urun_adi=a
-    ali.urun_magazasi=b
-    ali.urun_miktari=c
-    ali.urun_fiyati=d
-    ali.urun_tarihi=e
-    ali.ekle()
 end
--- program.rb
+++ program.rb
@@ -1,17 +1,42 @@
 module Alisveris_listesi
   class Urun
     DOSYA_ADI="../DB/almadan_once_liste"
-    attr_accessor :urun_adi, :urun_magazasi, :urun_miktari, :urun_fiyati, :urun_tarihi
+    DOSYA_ADI2="../DB/aldiktan_sonra_liste"
+    attr_accessor :urun_adi, :urun_magazasi, :urun_miktari, :urun_fiyati, :urun_tarihi, :gercek_fiyati
 
     def ekle()
       liste=[@urun_adi,@urun_magazasi,@urun_miktari,@urun_fiyati,@urun_tarihi]
       File.open(DOSYA_ADI,"a") do |urun|
-      urun.puts liste.join(",")
-    end    #do nun endi
+        urun.puts liste.join(",")
+      end
+    end
 
+    def gercek_fiyatlar()
+        liste=[@urun_adi,@urun_magazasi,@urun_miktari,@urun_fiyati,@urun_tarihi,@gercek_fiyati]
+        File.open(DOSYA_ADI2,"a") { |filemiz| filemiz.puts liste.join(",") }
+    end
 
+    class << self
+      def fiyat_toplama
+        @sayac=0
+        liste=[@urun_adi,@urun_magazasi,@urun_miktari,@urun_fiyati,@urun_tarihi]
 
+        File.open(DOSYA_ADI,"r") do |dosyamiz|
+          dosyamiz.readlines.reject { |satir| satir.empty? }.map {|eleman| eleman.split ','}.each { |eleman| @sayac += eleman[3].to_i }
+        end
 
-    end #def eklenin
-  end #classın
+        @sayac
+      end
+      def gercek_fiyat_toplama
+        @sayac=0
+        liste=[@urun_adi,@urun_magazasi,@urun_miktari,@urun_fiyati,@urun_tarihi,@gercek_fiyati]
+
+        File.open(DOSYA_ADI2,"r") do |dosyamiz|
+          dosyamiz.readlines.reject { |satir| satir.empty? }.map {|eleman| eleman.split ','}.each { |eleman| @sayac += eleman[5].to_i }
+        end
+        @sayac
+      end
+
+    end
+  end
 end #modulun
