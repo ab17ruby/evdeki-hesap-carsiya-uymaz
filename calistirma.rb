f7764f5423576dbfb05df7a62458b171c7dd0fc3
Himmet Emre Çimen <himmet172@gmail.com>
2017-02-07 13:52:40 +0200

    güncelleme input

--- calistirma.rb
+++ calistirma.rb
@@ -1,9 +1,25 @@
 require './program'
-
-ali=Alisveris_listesi::Urun.new
-ali.urun_adi="ffff"
-ali.urun_magazasi="aaaa"
-ali.urun_miktari="dasgfsaar"
-ali.urun_fiyati="fdadfdsafg"
-ali.urun_tarihi="lllll"
-ali.ekle()
+while true
+    print "urun adi giriniz"
+    a=gets.chomp()
+    print "urun magazasi giriniz"
+    b=gets.chomp()
+    print "urun miktari girinz"
+    c=gets.chomp()
+    print "urun fiyati giri,niz "
+    d=gets.chomp()
+    print " "
+    e=gets.chomp()
+    print "1,2"
+    adli=gets.chomp()
+    if adli== "2"
+      break
+    end
+    ali=Alisveris_listesi::Urun.new
+    ali.urun_adi=a
+    ali.urun_magazasi=b
+    ali.urun_miktari=c
+    ali.urun_fiyati=d
+    ali.urun_tarihi=e
+    ali.ekle()
+end
